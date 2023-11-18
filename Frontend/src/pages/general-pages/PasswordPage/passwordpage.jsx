8e0de8315068cd7da69a75fd97a5ec70739085fc
Maymoona-Ayoub <72512261+Maymoona12@users.noreply.github.com>
2023-11-18 16:26:10 +0200

    new pages

---
+++ Frontend/src/pages/general-pages/PasswordPage/passwordpage.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+const ForgotPassword = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Implement your password recovery logic here
+    console.log('Password recovery logic goes here');
+  };
+
+  return (
+    <div style={{paddingLeft:'450px'}}>
+      <Typography component="h1" variant="h4" style={{fontFamily:'serif'}}>
+        Forgot Password
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+          Send Recovery Email
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default ForgotPassword;
