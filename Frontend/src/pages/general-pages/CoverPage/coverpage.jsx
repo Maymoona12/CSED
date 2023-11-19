28ee66c6a7b3601127093a04e6ff273c7bdcc6a1
Maymoona-Ayoub <72512261+Maymoona12@users.noreply.github.com>
2023-11-19 23:45:49 +0200

    image add

--- Frontend/src/pages/general-pages/CoverPage/coverpage.jsx
+++ Frontend/src/pages/general-pages/CoverPage/coverpage.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
@@ -6,9 +6,30 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
+const images = [
+  "Images/image1.jpg",
+  "Images/image2.jpg",
+  "Images/image3.jpg",
+  // Add more image paths as needed
+];
+
 export default function Coverpage() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    // Change the image every 5 seconds
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(intervalId); // Cleanup interval on component unmount
+
+  }, []);
+
+  const currentImage = images[currentImageIndex];
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1, display: "flex", flexDirection: "column", alignItems: "center" }}>
       <AppBar
         position="static"
         style={{ position: "fixed", top: 0, zIndex: 1000 }}
@@ -37,18 +58,29 @@ export default function Coverpage() {
           </Link>
         </Toolbar>
       </AppBar>
-      <Typography
-        variant="h1"
-        component="div"
-        sx={{
-          fontFamily: "Brush Script MT",
-          color: "black",
-          textAlign: "center",
-          margin: "250px",
-        }}
-      >
-        Welcome to the CSED Community
-      </Typography>
+      <Box sx={{ flex: 1, textAlign: "center", marginTop: "80px" }}>
+        <img
+          src={currentImage}
+          alt="Slideshow"
+          style={{
+            width: "100%", // Adjust the width as needed
+            height: "auto",
+            display: "block",
+            margin: "0 auto",
+          }}
+        />
+        <Typography
+          variant="h1"
+          component="div"
+          sx={{
+            fontFamily: "Brush Script MT",
+            color: "black",
+            margin: "20px", // Adjust the margin as needed
+          }}
+        >
+          Welcome to the CSED Community
+        </Typography>
+      </Box>
     </Box>
   );
 }
