62cafb8d848ff3524905c8d73879d8da478d3193
Maymoona-Ayoub <72512261+Maymoona12@users.noreply.github.com>
2023-12-16 15:13:59 +0200

    edit change password

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -302,33 +302,33 @@
 
 // Student-Page:
 
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import StudentPage from "./pages/UsersPage/StudentPage/StudentPage";
-import Home from "./pages/PostAnnouncement/Home";
-import LecturerProfiles from "./pages/Lecturer-Profiles/lecturerprofile";
-import FoldersPage from "./pages/photo-archive/foldersPage";
-import PhotosPage from "./pages/photo-archive/photospage";
-import ChangePassword from "./pages/UsersPage/StudentPage/ChangePassword";
+// import React, { useState, useEffect } from "react";
+// import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+// import StudentPage from "./pages/UsersPage/StudentPage/StudentPage";
+// import Home from "./pages/PostAnnouncement/Home";
+// import LecturerProfiles from "./pages/Lecturer-Profiles/lecturerprofile";
+// import FoldersPage from "./pages/photo-archive/foldersPage";
+// import PhotosPage from "./pages/photo-archive/photospage";
+// import ChangePassword from "./pages/UsersPage/StudentPage/ChangePassword";
 
-const App = () => {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<StudentPage />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="/LecturersProfile" element={<LecturerProfiles />} />
-          <Route path="/ArchivePage" element={<FoldersPage />} />
-          <Route path="/PhotosPage/:folderId" element={<PhotosPage />} />
-          <Route path="/ChangePassword" element={<ChangePassword />} />
-        </Routes>
-      </Router>
-    </>
-  );
-};
+// const App = () => {
+//   return (
+//     <>
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<StudentPage />} />
+//           <Route path="/Home" element={<Home />} />
+//           <Route path="/LecturersProfile" element={<LecturerProfiles />} />
+//           <Route path="/ArchivePage" element={<FoldersPage />} />
+//           <Route path="/PhotosPage/:folderId" element={<PhotosPage />} />
+//           <Route path="/ChangePassword" element={<ChangePassword />} />
+//         </Routes>
+//       </Router>
+//     </>
+//   );
+// };
 
-export default App;
+// export default App;
 //__________________________________________________________________________
 
 // import React, { useState, useEffect } from "react";
