dbcdb32ae44d97e60a8641e4fdfae67c7d250aac
fatimaomar53 <asfatima5321@gmail.com>
2023-12-22 00:49:33 +0300

    edit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -306,32 +306,32 @@
 // // export default App;
 // //___________________________________________________________
 
-// // Lecturer-Page:
+// Lecturer-Page:
 
-// // import React, { useState, useEffect } from "react";
-// // import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-// // import LecturerPage from "./pages/UsersPage/Lecturer-Page/lecturerpage";
-// // import PostAnnouncementPage from "./pages/PostAnnouncement/PostAnnouncement";
-// // import LecturerProfiles from "./pages/Lecturer-Profiles/lecturerprofile";
-// // import Appointment from "./pages/Appointment/Appointment";
-// // import FoldersPage from "./pages/photo-archive/foldersPage";
-// // import PhotosPage from "./pages/photo-archive/photospage";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import LecturerPage from "./pages/UsersPage/Lecturer-Page/lecturerpage";
+import PostAnnouncementPage from "./pages/PostAnnouncement/PostAnnouncement";
+import LecturerProfiles from "./pages/Lecturer-Profiles/lecturerprofile";
+import Appointment from "./pages/Appointment/Appointment";
+import FoldersPage from "./pages/photo-archive/foldersPage";
+import PhotosPage from "./pages/photo-archive/photospage";
 
-// // const App = () => {
-// //   return (
-// //     <>
-// //       <Router>
-// //         <Routes>
-// //           <Route path="/" element={<LecturerPage />} />
-// //           <Route path="/PostAnnouncement" element={<PostAnnouncementPage />} />
-// //           <Route path="/LecturersProfile" element={<LecturerProfiles />} />
-// //           <Route path="/Add Appointment" element={<Appointment />} />
-// //           <Route path="/ArchivePage" element={<FoldersPage />} />
-// //           <Route path="/PhotosPage/:folderId" element={<PhotosPage />} />
-// //         </Routes>
-// //       </Router>
-// //     </>
-// //   );
-// // };
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LecturerPage />} />
+          <Route path="/PostAnnouncement" element={<PostAnnouncementPage />} />
+          <Route path="/LecturersProfile" element={<LecturerProfiles />} />
+          <Route path="/AddAppointment" element={<Appointment />} />
+          <Route path="/ArchivePage" element={<FoldersPage />} />
+          <Route path="/PhotosPage/:folderId" element={<PhotosPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
-// // export default App;
+export default App;
