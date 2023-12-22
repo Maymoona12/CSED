de355c49164a4bea63726d361f294a8eac3bcab0
fatimaomar53 <asfatima5321@gmail.com>
2023-12-23 01:29:37 +0300

    add student profile

---
+++ Frontend/src/pages/UsersPage/StudentPage/StudentProfile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./style.css";
+
+const StudentProfile = () => {
+  return (
+    <div className="upc">
+      <div className="gradiant"></div>
+      <div className="profile-down">
+        <img src="CoverImages/image1.jpg" alt="USER" />
+        <div className="profile-title">Fatima Omar</div>
+        <div className="profile-description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex numquam
+          asperiores dolorum eligendi eius. Iure ut similique est nesciunt at
+          beatae hic inventore accusantium dolor ea aperiam id, molestias
+          soluta?
+        </div>
+        <div className="profile-button"></div>
+      </div>
+    </div>
+  );
+};
+
+export default StudentProfile;
