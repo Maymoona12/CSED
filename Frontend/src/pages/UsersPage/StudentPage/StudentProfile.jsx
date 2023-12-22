7a370006ac80e05776317eaa25dca1b9f8354e97
fatimaomar53 <asfatima5321@gmail.com>
2023-12-23 02:45:24 +0300

    add student profile page

--- Frontend/src/pages/UsersPage/StudentPage/StudentProfile.jsx
+++ Frontend/src/pages/UsersPage/StudentPage/StudentProfile.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import "./style.css";
+import "./StudentProfile.css";
+import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
 
 const StudentProfile = () => {
   return (
     <div className="upc">
       <div className="gradiant"></div>
       <div className="profile-down">
-        <img src="CoverImages/image1.jpg" alt="USER" />
+        <div className="add-photo-container">
+          <AddAPhotoIcon className="add-photo-icon" />
+        </div>
+        <img src="Images/hamood1.jpg" alt="USER" />
         <div className="profile-title">Fatima Omar</div>
         <div className="profile-description">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex numquam
@@ -14,7 +18,9 @@ const StudentProfile = () => {
           beatae hic inventore accusantium dolor ea aperiam id, molestias
           soluta?
         </div>
-        <div className="profile-button"></div>
+        <div className="profile-button">
+          <a href="#">Change Password</a>
+        </div>
       </div>
     </div>
   );
