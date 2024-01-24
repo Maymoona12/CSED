4344fcd6b3a349a46b4582818e080e25c6ddac74
fatimaomar53 <asfatima5321@gmail.com>
2024-01-24 22:01:34 +0300

    edittt

--- Frontend/src/pages/Appointment/indexBookAppointment.js
+++ Frontend/src/pages/Appointment/indexBookAppointment.js
@@ -1,6 +1,6 @@
 import axiosInstance from "../../api/index";
 
-export const bookedAppointments = async () => {
-  const url = `/doctor_appointments`;
+export const bookedAppointments = async (id) => {
+  const url = `/doctor_appointments/${id}`;
   return axiosInstance.get(url).then((response) => response.data);
 };
